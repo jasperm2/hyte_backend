e2882e896b317835370d3cf66b2bd216e640fa9b
Jasper Mustasilta <zipuli69@gmail.com>
2026-02-23 06:26:10 +0200

    commit

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,73 @@
+// HUOM: mokkidata on poistettu modelista
+//import users from '../models/user-model.js';
+
+import jwt from 'jsonwebtoken';
+import {findUserByUsername} from '../models/user-model.js';
+
+// TODO: lisää tietokantafunktiot user modeliin
+// ja käytä niitä täällä
+
+// TODO: refaktoroi tietokantafunktiolle
+const getUsers = (req, response) => {
+  // ÄLÄ IKINÄ lähetä salasanoja HTTP-vastauksessa
+  for (let i = 0; i < users.length; i++) {
+    delete users[i].password;
+    // kaikki emailit sensuroitu esimerkki
+    // users[i].email = 'sensored';
+  }
+  response.json(users);
+};
+
+// TODO: getUserById
+// TODO: putUserById
+// TODO: deleteUserById
+
+// Käyttäjän lisäys (rekisteröityminen)
+// TODO: refaktoroi tietokantafunktiolle
+const postUser = (pyynto, vastaus) => {
+  const newUser = pyynto.body;
+  // Uusilla käyttäjillä pitää olla kaikki vaaditut ominaisuudet tai palautetaan virhe
+  // itse koodattu erittäin yksinkertainen syötteen validointi
+  if (!(newUser.username && newUser.password && newUser.email)) {
+    return vastaus.status(400).json({error: 'required fields missing'});
+  }
+
+  // HUOM: ÄLÄ ikinä loggaa käyttäjätietoja ensimmäisten pakollisten testien jälkeen!!! (tietosuoja)
+  //console.log('registering new user', newUser);
+  const newId = users[users.length - 1].id + 1;
+  // luodaan uusi objekti, joka sisältää id-ominaisuuden ja kaikki newUserObjektin
+  // ominaisuudet ja lisätään users-taulukon loppuun
+  users.push({id: newId, ...newUser});
+  delete newUser.password;
+  // console.log('users', users);
+  vastaus.status(201).json({message: 'new user added', user_id: newId});
+};
+
+// Tietokantaversio valmis
+const postLogin = async (req, res) => {
+  const {username, password} = req.body;
+  // haetaan käyttäjä-objekti käyttäjän nimen perusteella
+  const user = await findUserByUsername(username);
+  //console.log('postLogin user from db', user);
+  if (user) {
+    if (user.password === password) {
+      delete user.password;
+      // generate & sign token using a secret and expiration time
+      // read from .env file
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+      return res.json({message: 'login ok', user, token});
+    }
+    return res.status(403).json({error: 'invalid password'});
+  }
+  res.status(404).json({error: 'user not found'});
+};
+
+// Get user information stored inside token
+const getMe = (req, res) => {
+  res.json(req.user);
+};
+
+
+export {getUsers, postUser, postLogin, getMe};
---
+++ src/routes/user-router.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import {getMe, getUsers, postLogin, postUser} from '../controllers/user-controller.js';
+import {authenticateToken} from '../middlewares/authentication.js';
+
+const userRouter = express.Router();
+
+// Users resource endpoints
+userRouter.route('/')
+// GET all users
+.get(getUsers)
+// POST new user
+.post(postUser);
+
+// POST user login
+userRouter.post('/login', postLogin);
+
+// Get user info based on token
+userRouter.get('/me', authenticateToken, getMe);
+
+
+// TODO: get user by id
+// app.get('/api/users/:id');
+// TODO: put user by id
+// TODO: delete user by id
+
+export default userRouter;
